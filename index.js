8444d3dac28526920f57bf160a59120e2464a117
Kahlia Pinkins <kahliapinkins123@gmail.com>
2021-11-05 00:44:50 -0400

    commit tree e02520bab2ce280b7f53488c66b358a11ee313c8

--- index.js
+++ index.js
@@ -21,3 +21,80 @@ const allWagesFor = function () {
     return payable
 }
 
+function createEmployeeRecord(array){
+    const employeeRecord = {};
+    employeeRecord.firstName = array[0];
+    employeeRecord.familyName = array[1];
+    employeeRecord.title = array[2];
+    employeeRecord.payPerHour = array[3];
+    employeeRecord.timeInEvents= [];
+    employeeRecord.timeOutEvents = [];
+
+    return employeeRecord;
+
+}
+
+function createEmployeeRecords(arrayOfArrays){
+    const arrayOfObjects = [];
+    arrayOfArrays.map(function(array){
+            arrayOfObjects.push(createEmployeeRecord(array));      
+    })
+
+    return arrayOfObjects;
+}
+
+function createTimeInEvent(dateStamp){
+    const [date,hour] = dateStamp.split(' ');
+
+    const obj = {
+        type: 'TimeIn',
+        hour: parseInt(hour),
+        date: date
+    }
+
+    this.timeInEvents.push(obj);
+
+    return this;
+}
+
+function createTimeOutEvent(dateStamp){
+    const [date,hour] = dateStamp.split(' ');
+
+    const obj = {
+        type: 'TimeOut',
+        hour: parseInt(hour),
+        date: date
+    }
+
+    this.timeOutEvents.push(obj);
+
+    return this;
+}
+
+
+
+function hoursWorkedOnDate(thisDate){
+    const timeIn = this.timeInEvents.find(timeIn => timeIn.date === thisDate)
+    const timeOut = this.timeOutEvents.find(timeOut => timeOut.date === thisDate)
+
+    return (timeOut.hour - timeIn.hour)/100;
+
+}
+
+function wagesEarnedOnDate(thisDate){
+    return hoursWorkedOnDate.call(this,thisDate)*this.payPerHour;
+}
+
+function findEmployeeByFirstName(srcArray,name){
+    return srcArray.find(emp=> name === emp.firstName);
+}
+
+function calculatePayroll(emps){
+    let total = 0;
+
+    emps.map(function(emp){
+       total = total + allWagesFor.call(emp);
+    })
+
+    return total;
+}
